138f34519d5b19408eecfff9409541a66e4b9f5d
Minotauro49 <34907795+Minotauro49@users.noreply.github.com>
2018-12-16 20:02:47 +1100

    Able to receive data successfully from Ajax Post req

--- Back_End/Main.py
+++ Back_End/Main.py
@@ -3,16 +3,14 @@ from flask_restful import Resource, Api
 from json import dumps
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class Main(Resource):
-    def get(self):
-        return { "name":"John", "age":30, "city":"New York" } # Fetches first column that is Employee ID
+@app.route('/visualiser', methods=['POST'])
+def Visualiser():
+    print(request.json)
+    return "json.dumps(request.json)"
 
 
-
-api.add_resource(Main, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
