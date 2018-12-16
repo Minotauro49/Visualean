39148dcd5b7d923071720b990a2588ef42036c34
Minotauro49 <34907795+Minotauro49@users.noreply.github.com>
2018-12-16 20:01:55 +1100

    able to send data to Py API

--- Front_End/Engine.js
+++ Front_End/Engine.js
@@ -1,47 +1,19 @@
-var app = angular.module("visualearnEngine", []);
-app.controller('myCtrl', function ($scope, $http) {
-    $http.get("http://127.0.0.1:5000/").then(function(response) {
-
-        // $("#UserField").click(function () {
-        $scope.visualearnEngineData = response.data;
-    });
-});
-
-
-
 
 $(document).ready(function () {
-    $("#UserField").click(function() {
-        getdata();
+    $("#search").click(function() {
+      getdata();
     });
 });
 
-
-
-
-// var update = setInterval(function () {
-//     // posts Email
-//     if (connection == true && checked == true) {
-//         $.post("http://localhost:8080/mailsender", jsonUser);
-//         checked = false;
-//         console.log("Posted")
-//     }
-
-
+//https://stackoverflow.com/questions/48463807/sending-a-post-request-to-my-restful-apipython-flask-but-receiving-a-get-requ
 function getdata(){
-    // $.ajax({
-    //   xhrFields: { withCredentials: true },
-    //   type: "GET",
-    //   url: "http://127.0.0.1:5000/",
-    //   dataType: "jsonp",
-    //   success: function (data) {
-    //       console.log(data);
-    //   },
-    //   error: function (error) {
-    //       console.log("FAIL....=================");
-
-    //   }}).done(function(data) {
-    //   // data = JSON.parse(data);
-    //   console.log("data");
-    // });
+    $.ajax({
+      type: "POST",
+      url: "http://127.0.0.1:5000/visualiser",
+      data: JSON.stringify({VisualiseData:$("textarea").val()}),
+      contentType: "application/json;charset=UTF-8",
+      success: function(result) {
+        console.log("Successful Send!");
+      }
+    });
 }
